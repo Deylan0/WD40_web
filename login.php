d3a7b20324f6fd28272db9c94d6bcddab864ebff
Miłosz <miloszobroslak6@gmail.com>
2023-11-30 21:37:19 +0100

    background img | secret tutel | overal fix

---
+++ login.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style tutel.css" type="text/css"/>
+</head>
+<body>
+    <div id="tutel"></div>
+        <div id="text">Wielki Tutel prosi o hasło:</div>
+        <form></form>
+    
+</body>
+</html>
