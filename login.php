0f0c9bf0f0296fa577bfa4e0c514d791b72b3406
milosz <miloszobroslak6@gmail.com>
2024-02-19 21:16:16 +0100

    created login system(not working)

--- login.php
+++ login.php
@@ -1,15 +1,8 @@
-<!DOCTYPE html>
-<html lang="pl">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link rel="stylesheet" href="style tutel.css" type="text/css"/>
-</head>
-<body>
-    <div id="tutel"></div>
-        <div id="text"></div>
-        <form></form>
+<?php 
+    session_start();
+    $conn = new mysqli("localhost","root", "","WD40");
+    $sql = "SELECT login FROM Logins;";
+    $result = $conn->query($sql);
+    $row = $result->fetch_all();
     
-</body>
-</html>
+?>
---
+++ loginForm.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+    <link rel="stylesheet" href="styleLogin.css" type="text/css"/>
+</head>
+<body>   
+    <main>
+
+        <div id="content">
+            <?php
+                $loginErr = "";
+                $login =  "";
+
+                if ($_SERVER["REQUEST_METHOD"] == "POST") {
+                    if (empty($_POST["login"])) {
+                        $loginErr = "login is required";
+                    } else {
+                        $login = test_input($_POST["login"]);
+                    }
+                }
+
+                function test_input($data) {
+                $data = trim($data);
+                $data = stripslashes($data);
+                $data = htmlspecialchars($data);
+                return $data;
+                }
+                ?>
+
+            <form method="post" action="<?php echo htmlspecialchars("login.php");?>">
+                Login: <input type="text" name="login">
+                <span class="error"><?php if($login == "login is required"){
+                    echo *login is required
+                }?></span>
+                <input type="submit" name="submit" value="Submit">
+            </form>
+        </div>
+
+    </main>
+</body>
+</html>
